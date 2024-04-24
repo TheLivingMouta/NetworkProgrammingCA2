33d0f08e0fe0c5c477f6a24eecdfae0384a1225b
OranBrady <oranbrady01@gmail.com>
2024-04-24 15:49:51 +0100

    Threading done
    YEET!

--- src/main/java/Server/FilmClientHandler.java
+++ src/main/java/Server/FilmClientHandler.java
@@ -6,10 +6,14 @@ import business.User;
 import business.UserManager;
 import protocol.FilmService;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
+import java.util.Scanner;
 
-public class FilmClientHandler implements Runnable{
+public class FilmClientHandler implements Runnable {
 
     private Socket dataSocket;
     private FilmManager fm;
@@ -25,7 +29,78 @@ public class FilmClientHandler implements Runnable{
 
     public void run() {
 
+        try (ServerSocket ls = new ServerSocket(FilmService.PORT)) {
+            fm = new FilmManager();
+
+            boolean validSession = true;
+            while (validSession) {
+                Socket ds = ls.accept();
+
+                try (Scanner clientInput = new Scanner(ds.getInputStream());
+                     PrintWriter clientOutput = new PrintWriter(ds.getOutputStream())) {
+                    String request = clientInput.nextLine();
+                    System.out.println(ds.getInetAddress() + ":" + ds.getPort());
+
+                    String response = null;
+
+                    String[] components = request.split(FilmService.DELIMITER);
+                    switch (components[0]) {
+                        case FilmService.EXIT:
+                            if (components.length == 1) {
+                                validSession = false;
+                                response = FilmService.GOODBYE;
+                            } else {
+                                response = FilmService.INVALID_REQUEST;
+                            }
+                            break;
+                        case FilmService.SHUTDOWN:
+                            if (components.length == 1 && lu.getAdminStatus() == 1) {
+                                response = FilmService.SHUTTING_DOWN;
+                            } else {
+                                response = FilmService.INSUFFICIENT_PERMISSIONS;
+                            }
+                            break;
+                        case FilmService.REGISTER:
+                            response = register(components);
+                            break;
+                        case FilmService.LOGIN:
+                            response = login(components);
+                            break;
+                        case FilmService.LOGOUT:
+                            response = logout(components);
+                            break;
+                        case FilmService.RATING:
+                            response = rateFilm(components);
+                            break;
+                        case FilmService.SEARCH_TITLE:
+                            response = searchByName(components);
+                            break;
+                        case FilmService.SEARCH_GENRE:
+                            response = searchByGenre(components);
+                            break;
+                        case FilmService.ADD_FILM:
+                            response = addFilm(components);
+                            break;
+                        case FilmService.REMOVE_FILM:
+                            response = removeFilm(components);
+                            break;
+                        default:
+                            response = FilmService.INVALID_REQUEST;
+                    }
+
+                    clientOutput.println(response);
+                    clientOutput.flush();
+
+                }
+
+            }
+        }catch (IOException e) {
+            System.out.println("IOException occurred on data socket when communicating with " + dataSocket.getInetAddress() + ":" + dataSocket.getPort());
+            System.out.println(e.getMessage());
+        }
     }
+
+
     private String login(String[] components) {
 
         String response;
--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -5,6 +5,7 @@ import business.FilmManager;
 import business.User;
 import business.UserManager;
 import protocol.FilmService;
+import server.FilmClientHandler;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -24,79 +25,26 @@ public class Server {
 
         try (ServerSocket ls = new ServerSocket(FilmService.PORT)) {
             fm = new FilmManager();
+            um = new UserManager();
 
             boolean validSession = true;
-            while (validSession) {
+            while (true) {
                 Socket ds = ls.accept();
+                FilmClientHandler filmHandler = new FilmClientHandler(ds, fm, um);
+                Thread worker = new Thread(filmHandler);
+                worker.start();
 
-                try (Scanner clientInput = new Scanner(ds.getInputStream());
-                     PrintWriter clientOutput = new PrintWriter(ds.getOutputStream())) {
-                    String request = clientInput.nextLine();
-                    System.out.println(ds.getInetAddress() + ":" + ds.getPort());
-
-                    String response = null;
-
-                    String[] components = request.split(FilmService.DELIMITER);
-                    switch (components[0]) {
-                        case FilmService.EXIT:
-                            if (components.length == 1) {
-                                validSession = false;
-                                response = FilmService.GOODBYE;
-                            } else {
-                                response = FilmService.INVALID_REQUEST;
-                            }
-                            break;
-                        case FilmService.SHUTDOWN:
-                            if (components.length == 1 && lu.getAdminStatus() == 1) {
-                                response = FilmService.SHUTTING_DOWN;
-                            } else {
-                                response = FilmService.INSUFFICIENT_PERMISSIONS;
-                            }
-                            break;
-                        case FilmService.REGISTER:
-                            response = register(components);
-                            break;
-                        case FilmService.LOGIN:
-                            response = login(components);
-                            break;
-                        case FilmService.LOGOUT:
-                            response = logout(components);
-                            break;
-                        case FilmService.RATING:
-                            response = rateFilm(components);
-                            break;
-                        case FilmService.SEARCH_TITLE:
-                            response = searchByName(components);
-                            break;
-                        case FilmService.SEARCH_GENRE:
-                            response = searchByGenre(components);
-                            break;
-                        case FilmService.ADD_FILM:
-                            response = addFilm(components);
-                            break;
-                        case FilmService.REMOVE_FILM:
-                            response = removeFilm(components);
-                            break;
-                        default:
-                            response = FilmService.INVALID_REQUEST;
-                    }
-
-                    clientOutput.println(response);
-                    clientOutput.flush();
-
-                }
 
             }
+                } catch (BindException e) {
+                    System.out.println("BindException occurred when attempting to bind to port " + FilmService.PORT);
+                    System.out.println(e.getMessage());
 
-        } catch (BindException e) {
-            System.out.println("BindException occurred when attempting to bind to port " + FilmService.PORT);
-            System.out.println(e.getMessage());
         } catch (IOException e) {
             System.out.println("IOException occurred on server socket");
             System.out.println(e.getMessage());
         }
-
-
     }
 }
+
 
